0b7d8b2597cffb4d00cd0f5e6a82c2f3123c7338
gauriiii13 <101903173+gauriiii13@users.noreply.github.com>
2023-08-15 14:44:05 +0530

    Update chatgptchatbot.py

--- TASK1/chatgptchatbot.py
+++ TASK1/chatgptchatbot.py
@@ -1,8 +1,7 @@
 import openai
 import streamlit as st
-import os
 
-openai.api_key = os.environ.get("OPENAI_API_KEY")
+openai.api_key = st.secrets("API_KEY")
 
 messages = [{"role": "system", "content": "You are a psychologist"}]
 
